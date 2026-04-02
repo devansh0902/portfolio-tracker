66d0acc51a21257e63e4ac6676eab90edb1558a2
devansh0902 <devanshgupta0902@gmail.com>
2026-04-02 22:52:31 +0530

    Fix database setup and add working register & login APIs

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,15 +1,37 @@
 from flask import Blueprint, request, jsonify
 from app.extensions import db
 from app.models.user import User
-auth_bp= Blueprint('auth', __name__)
-@auth_bp.route("/register",methods=["POST"])
+
+auth_bp = Blueprint("auth", __name__)
+
+# REGISTER
+@auth_bp.route("/register", methods=["POST"])
 def register():
-    data=request.get_json()
-    user=User(
-    name=data["name"],
-    email=data["email"], 
-    password=data["password"]
+    data = request.get_json()
+
+    user = User(
+        name=data.get("name"),
+        email=data.get("email"),
+        password=data.get("password")
     )
+
     db.session.add(user)
     db.session.commit()
-    return jsonify({"message": "User registered successfully"}), 201
+
+    return jsonify({"message": "User registered successfully"})
+
+
+# LOGIN
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user and user.password == password:
+        return jsonify({"message": "Login successful"})
+
+    return jsonify({"message": "Invalid email or password"}), 401
